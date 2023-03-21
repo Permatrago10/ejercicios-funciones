ee00339a19fb0041eb214852a3334b65d04ab326
Permatrago10 <114485628+Permatrago10@users.noreply.github.com>
2023-03-21 13:40:39 +0100

    Add files via upload

---
+++ división.js
@@ -0,0 +1,10 @@
+var a_1 = 9;
+var a_2 = 63;
+
+function division(a, b){
+    if(b == 0){
+    console.log("no se puede dividir por 0")
+    }else{
+        return a/b
+    }
+}
---
+++ edad.js
@@ -0,0 +1,18 @@
+function edad(numero){
+if(numero < 13){
+    return "niño";
+}else if(numero>=13 && numero<18){
+    return "adolescente";
+
+}else if(numero>=19 && numero<=64){
+    return "niño";
+
+}else{
+    return "mayor";
+
+
+}
+
+}
+  
+
